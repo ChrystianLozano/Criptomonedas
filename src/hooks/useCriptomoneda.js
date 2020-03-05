44282b156b4ba85bd34ef9c5d98f010b36b90ae0
Chrystian Lozano <chrystianlozano@icloud.com>
2020-03-05 10:37:50 -0600

    Mostrando los datos de la api en el select

--- src/hooks/useCriptomoneda.js
+++ src/hooks/useCriptomoneda.js
@@ -22,7 +22,7 @@ const Select = styled.select`
 `;
 
 const useCriptomoneda = (label, stateInicial, opciones) => {
-  console.log(opciones)
+  // console.log(opciones)
 
 
   //state de nuestro custom hook
@@ -33,13 +33,11 @@ const useCriptomoneda = (label, stateInicial, opciones) => {
       <Label>{label}</Label>
       <Select onChange={e => actualizarState(e.target.value)} value={state}>
         <option value="">Seleccione</option>
-        {
-        // opciones.map(opcion => (
-        //   <option key={opcion.codigo} value={opcion.codigo}>
-        //     {opcion.nombre}
-        //   </option>
-        // ))
-        }
+        {opciones.map(opcion => (
+          <option key={opcion.CoinInfo.Id} value={opcion.CoinInfo.Name}>
+            {opcion.CoinInfo.FullName}
+          </option>
+        ))}
       </Select>
     </Fragment>
   );

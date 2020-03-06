fc2cad8e5cd1a82b832a6d4fcb396452094205dc
Chrystian Lozano <chrystianlozano@icloud.com>
2020-03-06 10:42:40 -0600

    Dando estilos

--- src/components/Cotizacion.js
+++ src/components/Cotizacion.js
@@ -1,27 +1,48 @@
 import React from 'react';
+import styled from '@emotion/styled'
+
+const ResultadoDiv = styled.div`
+color: #FFF;
+font-family: Arial;
+`
+
+const Parrafo = styled.p`
+font-size: 18px;
+    span {
+        font-weight: bold;
+    }
+`;
+
+const Precio = styled.p`
+  font-size: 30px;
+    span {
+    font-weight: bold;
+  }
+`;
+
 const Cotizacion = ({resultado}) => {
     if(Object.keys(resultado).length === 0) return null
 
     console.log(resultado)
 
     return (
-      <div>
-        <p>
+      <ResultadoDiv>
+        <Precio>
           El precio es: <span>{resultado.PRICE}</span>
-        </p>
-        <p>
+        </Precio>
+        <Parrafo>
           Precio más alto del día: <span>{resultado.HIGHDAY}</span>
-        </p>
-        <p>
+        </Parrafo>
+        <Parrafo>
           Precio más bajo del: <span>{resultado.LOWDAY}</span>
-        </p>
-        <p>
+        </Parrafo>
+        <Parrafo>
           Variación últimas 24 horas: <span>{resultado.CHANGEPCT24HOUR}</span>
-        </p>
-        <p>
+        </Parrafo>
+        <Parrafo>
           Ultima Actualización: <span>{resultado.LASTUPDATE}</span>
-        </p>
-      </div>
+        </Parrafo>
+      </ResultadoDiv>
     );
 }
  

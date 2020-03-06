023c9d6527318c9f08773ae0e378eae646e1c6f1
Chrystian Lozano <chrystianlozano@icloud.com>
2020-03-06 10:28:53 -0600

    Mostrando el resultado en UI

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import styled from '@emotion/styled'
 import imagen from './cryptomonedas.png'
 import Formulario from './components/Formulario'
+import Cotizacion from "./components/Cotizacion";
 import axios from 'axios'
 
 const Contenedor = styled.div`
@@ -45,6 +46,7 @@ function App() {
 
   const [moneda, guardarMoneda] = useState('')  
   const [criptomoneda, guardarCriptomoneda] = useState('')
+  const [resultado, guardarResultado] = useState({})
 
   useEffect(() => {
     if(moneda === ''){
@@ -57,7 +59,7 @@ function App() {
        `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
       const resultado = await axios.get(url);
 
-      console.log(resultado)
+      guardarResultado(resultado.data.DISPLAY[criptomoneda][moneda])
     };
 
     consultarAPI();
@@ -81,6 +83,10 @@ function App() {
         <Formulario 
           guardarMoneda = {guardarMoneda}
           guardarCriptomoneda = {guardarCriptomoneda}
+        />
+
+        <Cotizacion 
+          resultado = {resultado}
         />
       </div>
     </Contenedor>
